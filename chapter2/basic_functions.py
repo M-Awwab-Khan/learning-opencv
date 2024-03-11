97633f5ac4443b1f8a700233774bcc577d8da385
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-11 20:30:04 +0500

    gaussian blur

--- chapter2/basic_functions.py
+++ chapter2/basic_functions.py
@@ -3,6 +3,8 @@ import cv2
 img = cv2.imread('resources/lena.png')
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(img,(7, 7), 0)
 
 cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blurred Image",imgBlur)
 cv2.waitKey(0)
