ce25b76b4e85ccb6f165264b72fd3dfcd95ee02b
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-11 20:30:16 +0500

    edge detection

--- chapter2/basic_functions.py
+++ chapter2/basic_functions.py
@@ -4,7 +4,9 @@ img = cv2.imread('resources/lena.png')
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(img,(7, 7), 0)
+imgCanny = cv2.Canny(img, 150, 150)
 
 cv2.imshow("Gray Image",imgGray)
 cv2.imshow("Blurred Image",imgBlur)
+cv2.imshow("Canny Image",imgCanny)
 cv2.waitKey(0)
