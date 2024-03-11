7e1b4ea658361d7953722ade19f550c0a647755d
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-11 20:42:00 +0500

    resize image

---
+++ chapter3/cropandresize.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("Resources/lambo.png")
+print(img.shape)
+
+imgResize = cv2.resize(img,(300, 200))
+print(imgResize.shape)
+
+cv2.imshow("Image",img)
+cv2.imshow("Image Resize",imgResize)
+
+cv2.waitKey(0)
